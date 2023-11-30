3be2bca34fc110660c2902b1ccff2e083ad6fe0b
KazusaNakagawa <kazusanakagawa@gmail.com>
2023-11-30 22:22:28 +0900

    fix formatter

--- run_requests.sh
+++ run_requests.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 # 以下のようにすると、-query に指定したキーワードを変数に入れて実行できる
 # Usage: ./run_requests.sh
 
-for keyword in bluebook 花火 お祭り たこやき
-  do
-      docker-compose exec app python main.py -query $keyword -limit 50
-  done
+for keyword in bluebook 花火 お祭り たこやき; do
+	docker-compose exec app python main.py -query $keyword -limit 50
+done
