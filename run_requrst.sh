a44204a9577803981f07eafeede485bafff47192
KazusaNakagawa <kazusanakagawa@gmail.com>
2023-11-30 22:12:30 +0900

    fix: typo

--- run_requrst.sh
+++
@@ -1,9 +0,0 @@
-# 以下のようにすると、-query に指定したキーワードを変数に入れて、
-# それを使ってコマンドを実行できる
-# また、-limit に指定した数だけ、ループで回す
-# Usage: bash run_request.sh
-
-for keyword in bluebook 花火 お祭り たこやき
-  do
-      docker-compose exec app python main.py -query $keyword -limit 50
-  done
